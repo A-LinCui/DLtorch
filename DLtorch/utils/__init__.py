0da8e23be1ba4f6fabb5ab6dfff8458d68051b43
zhaojb17 <zhaojb17@mails.tsinghua.edu.cn>
2020-12-06 23:50:53 +0800

    Add new logger

--- DLtorch/utils/__init__.py
+++ DLtorch/utils/__init__.py
@@ -1,4 +1,4 @@
-from DLtorch.utils.logger import logger
+from aw_nas.utils.log import *
 from DLtorch.utils.torch_utils import *
 from DLtorch.utils.python_utils import *
 from DLtorch.utils.common_utils import *
