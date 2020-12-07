8843e94e79044631296a65ad91b2d1e75506d51f
zhaojb17 <zhaojb17@mails.tsinghua.edu.cn>
2020-12-07 23:32:20 +0800

    Add adversary

--- DLtorch/objective/__init__.py
+++ DLtorch/objective/__init__.py
@@ -1,3 +1,4 @@
 from DLtorch.objective.base import *
 from DLtorch.objective.classification_objective import *
 from DLtorch.objective.classification_adversarial_objective import *
+from DLtorch.objective.adversary import *
