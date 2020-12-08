3f588b9b0217b1e178497dbf5d27d8beaf6f8ffa
zhaojb17 <zhaojb17@mails.tsinghua.edu.cn>
2020-12-08 20:24:13 +0800

    Rename

--- DLtorch/__init__.py
+++ DLtorch/__init__.py
@@ -2,8 +2,9 @@ from __future__ import absolute_import
 
 from DLtorch import base
 from DLtorch import utils
-from DLtorch import models
-from DLtorch import datasets
+from DLtorch import model
+from DLtorch import dataset
+from DLtorch import trainer
 from DLtorch import objective
 from DLtorch import optimizer
 from DLtorch import lr_scheduler
