b9bb30fc1c0f3c92e0c5b5d6a7cbead6446f2d44
zhaojb17 <zhaojb17@mails.tsinghua.edu.cn>
2021-01-01 13:15:39 +0800

    Fix bugs

--- DLtorch/model/__init__.py
+++ DLtorch/model/__init__.py
@@ -2,6 +2,6 @@
 
 from DLtorch.model.base import BaseModel
 from DLtorch.model.DenseNet import CifarDenseNet
-from DLtorch.model.resnet import CifarResNet
+from DLtorch.model.ResNet import CifarResNet
 from DLtorch.model.LeNet import CifarLeNet, MNISTLeNet
 from DLtorch.model.WideResNet import CifarWideResNet
