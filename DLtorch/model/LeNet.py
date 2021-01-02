c593926d41e1539dfcec9c5ea6b6577f5013f7bc
zhaojb17 <zhaojb17@mails.tsinghua.edu.cn>
2021-01-02 14:32:29 +0800

    Fix bug of CifarLeNet

--- DLtorch/model/LeNet.py
+++ DLtorch/model/LeNet.py
@@ -16,13 +16,13 @@ class CifarLeNet(nn.Module, BaseModel):
         self,
         **kwargs
         ):
-        super(CifarLeNet, self).__init__()
+        nn.Module.__init__(self)
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
-        super(BaseModel).__init__(self)
+        BaseModel.__init__(self)
 
 
     def forward(self, x):
