7fd36af616bf0d8f38cdf3a2410e55445a279a49
zhaojb17 <zhaojb17@mails.tsinghua.edu.cn>
2021-01-03 14:30:39 +0800

    Add shuffleNet

--- DLtorch/model/__init__.py
+++ DLtorch/model/__init__.py
@@ -5,3 +5,4 @@ from DLtorch.model.DenseNet import CifarDenseNet
 from DLtorch.model.ResNet import CifarResNet
 from DLtorch.model.LeNet import CifarLeNet, MNISTLeNet
 from DLtorch.model.WideResNet import CifarWideResNet
+from DLtorch.model.ShuffleNet import CifarShuffleNet
