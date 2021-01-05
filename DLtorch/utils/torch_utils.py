27bfe18d9298fad99d6eda4508afa707e35b898f
zhaojb17 <zhaojb17@mails.tsinghua.edu.cn>
2021-01-05 19:34:27 +0800

    Fix bugs

--- DLtorch/utils/torch_utils.py
+++ DLtorch/utils/torch_utils.py
@@ -1,13 +1,14 @@
 # -*- coding:utf-8 -*-
 
 import warnings
+from collections import OrderedDict
 
 import pandas as pd
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torchviz import make_dot
-from torchstat import ModelStat
+from torchstat import ModelStat, ModelHook, StatTree, StatNode
 
 
 class CrossEntropyLabelSmooth(nn.Module):
@@ -65,6 +66,59 @@ def accuracy(outputs, targets, topk=(1,)):
 
 
 # ---- Tools for Model Analysis ----
+class _ModelHook(ModelHook):
+    def __init__(self, model, input_size, device):
+        super(_ModelHook, self).__init__(model, input_size)
+        self._model = model
+        x = torch.rand(1, *self._input_size).to(device)  # add module duration time
+        self._model.eval()
+        self._model(x)
+
+
+class _ModelStat(ModelStat):
+    def __init__(self, model, input_size, query_granularity=1):
+        super(_ModelStat, self).__init__(model, input_size, query_granularity)
+
+    def _analyze_model(self):
+        model_hook = _ModelHook(self._model, self._input_size)
+        leaf_modules = model_hook.retrieve_leaf_modules()
+        stat_tree = self.convert_leaf_modules_to_stat_tree(leaf_modules)
+        collected_nodes = stat_tree.get_collected_stat_nodes(self._query_granularity)
+        return collected_nodes
+
+    def convert_leaf_modules_to_stat_tree(self, leaf_modules):
+        assert isinstance(leaf_modules, OrderedDict)
+
+        create_index = 1
+        root_node = StatNode(name='root', parent=None)
+        for leaf_module_name, leaf_module in leaf_modules.items():
+            names = leaf_module_name.split('.')
+            for i in range(len(names)):
+                create_index += 1
+                stat_node_name = '.'.join(names[0:i+1])
+                parent_node = self.get_parent_node(root_node, stat_node_name)
+                node = StatNode(name=stat_node_name, parent=parent_node)
+                parent_node.add_child(node)
+                if i == len(names) - 1:  # leaf module itself
+                    input_shape = leaf_module.input_shape.numpy().tolist()
+                    output_shape = leaf_module.output_shape.numpy().tolist()
+                    node.input_shape = input_shape
+                    node.output_shape = output_shape
+                    node.Flops = leaf_module.Flops.numpy()[0]
+        return StatTree(root_node)
+    
+    def get_parent_node(self, root_node, stat_node_name):
+        assert isinstance(root_node, StatNode)
+        node = root_node
+        names = stat_node_name.split('.')
+        for i in range(len(names) - 1):
+            node_name = '.'.join(names[0:i+1])
+            child_index = node.find_child_index(node_name)
+            assert child_index != -1
+            node = node.children[child_index]
+        return node
+
+
 def get_params(model, only_trainable=False):
     """
     Get the parameter number of the model.
@@ -101,7 +155,7 @@ def get_flops(model, shape):
     For example, for a typical model trained on CIFAR-10, it should be (3, 32, 32)
     """
 
-    model_analyzer = ModelStat(model, shape) 
+    model_analyzer = _ModelStat(model, shape) 
     collected_nodes = model_analyzer._analyze_model()
     flops = 0
     for node in collected_nodes:
